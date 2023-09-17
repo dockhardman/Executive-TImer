59e316b8d1acff60568ca7896458e9f556aa4158
Allen Chou <f1470891079@gmail.com>
2023-09-17 20:39:36 +0800

    Initial Commit

---
+++ Executive TImer/Executive_TImerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Executive_TImerApp.swift
+//  Executive TImer
+//
+//  Created by Allen Chou on 2023/9/17.
+//
+
+import SwiftUI
+
+@main
+struct Executive_TImerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
